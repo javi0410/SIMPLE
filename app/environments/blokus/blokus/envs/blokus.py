a9f51650951fa65fc2b9349de64b3d2d5e2debb9
Javier Alcaide <javier.alcaide@bluetab.net>
2022-09-16 12:05:52 +0200

    test obs

--- app/environments/blokus/blokus/envs/blokus.py
+++ app/environments/blokus/blokus/envs/blokus.py
@@ -140,6 +140,8 @@ class BlokusEnv(gym.Env):
         legal_actions_r = np.concatenate(
             (np.array(self.legal_actions_uncached), np.zeros(99))
         )
+        print("LEGAL_ACTIONS ENV")
+        print(str(legal_actions_r))
         legal_actions_r.resize(10, 10, 22)
 
         out = np.concatenate((obs, legal_actions_r), axis=2)
